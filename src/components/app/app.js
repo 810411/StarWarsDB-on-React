d5ad7e018cbb3bc092d9ec92f622ac8e573cf49f
Grigorii Balabanov <810411@mail.ru>
2019-01-30 20:32:18 +0500

    0.5.1

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Route, Redirect, Switch} from "react-router-dom";
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
@@ -7,7 +7,13 @@ import ErrorBoundry from '../error-boundry';
 import SwapiService from '../../services/swapi-service';
 import DummySwapiService from '../../services/dummy-swapi-service';
 
-import {PeoplePage, PlanetsPage, StarshipsPage} from '../pages';
+import {
+  PeoplePage,
+  PlanetsPage,
+  StarshipsPage,
+  LoginPage,
+  SecretPage
+} from '../pages';
 import {SwapiServiceProvider} from '../swapi-service-context';
 
 import './app.css';
@@ -16,7 +22,14 @@ import StarshipDetails from "../sw-components/starship-details";
 export default class App extends Component {
 
   state = {
-    swapiService: new SwapiService()
+    swapiService: new SwapiService(),
+    isLoggedIn: false
+  };
+
+  onLogin = () => {
+    this.setState({
+      isLoggedIn: true
+    })
   };
 
   onServiceChange = () => {
@@ -30,6 +43,7 @@ export default class App extends Component {
   };
 
   render() {
+    const {isLoggedIn} = this.state;
 
     return (
       <ErrorBoundry>
@@ -39,11 +53,28 @@ export default class App extends Component {
               <Header onServiceChange={this.onServiceChange}/>
               <RandomPlanet/>
 
-              <Route path="/" render={() => <h2>Welcome to StarDB</h2>} exact/>
-              <Route path="/people" component={PeoplePage}/>
-              <Route path="/planets" component={PlanetsPage}/>
-              <Route path="/starships" component={StarshipsPage} exact/>
-              <Route path="/starships/:id" render={({match}) => <StarshipDetails itemId={match.params.id}/>}/>
+              <Switch>
+
+                <Route path="/" render={() => <h2>Welcome to StarDB</h2>} exact/>
+                <Route path="/people/:id?" component={PeoplePage}/>
+                <Route path="/planets" component={PlanetsPage}/>
+                <Route path="/starships" component={StarshipsPage} exact/>
+                <Route path="/starships/:id" render={({match}) => <StarshipDetails itemId={match.params.id}/>}/>
+                <Route path="/login"
+                       render={() =>
+                         <LoginPage
+                           isLoggedIn={isLoggedIn}
+                           onLogin={this.onLogin}
+                         />}/>
+                <Route path="/secret"
+                       render={() =>
+                         <SecretPage
+                           isLoggedIn={isLoggedIn}
+                         />}/>
+
+                <Route render={() => <h2 className="text-center">404. Page not found</h2>}/>
+
+              </Switch>
 
             </div>
           </BrowserRouter>
---
+++ src/components/pages/login-page.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Redirect} from "react-router-dom";
+
+const LoginPage = ({isLoggedIn, onLogin}) => {
+  if (isLoggedIn) {
+    return (
+      <Redirect to="/secret"/>
+    )
+  }
+
+  return (
+    <div className="jimbotron">
+      <p>Login to access secret page</p>
+      <button
+        className="btn btn-primary"
+        onClick={onLogin}
+      >
+        Login
+      </button>
+    </div>
+  )
+};
+
+export default LoginPage
